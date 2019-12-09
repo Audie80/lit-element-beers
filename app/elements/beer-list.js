f0f3bbd6fd9aff251549ddf6812b9bf8f3a5a268
Audie <audie.menut@gmail.com>
2019-12-09 16:13:08 +0100

    Step 6

--- app/elements/beer-list.js
+++ app/elements/beer-list.js
@@ -2,33 +2,7 @@ import { LitElement, html } from '../web_modules/lit-element.js';
 import './beer-list-item.js';
 import bootstrapStyle from '../web_modules/@granite-elements/granite-lit-bootstrap.js';
 
-const beers = [
-  {
-    "alcohol": 6.8,
-    "name": "Affligem Blond",
-    "description": "Affligem Blonde, the classic clear blonde abbey ale, with a gentle roundness and 6.8% alcohol. Low on bitterness, it is eminently drinkable."
-  },
-  {
-    "alcohol": 8.5,
-    "name": "Affligem Tripel",
-    "description": "The king of the abbey beers. It is amber-gold and pours with a deep head and original aroma, delivering a complex, full bodied flavour. Pure enjoyment! Secondary fermentation in the bottle."
-  },
-  {
-    "alcohol": 9.2,
-    "name": "Rochefort 8",
-    "description": "A dry but rich flavoured beer with complex fruity and spicy flavours."
-  },
-  {
-    "alcohol": 11.3,
-    "name": "Rochefort 10",
-    "description": "The top product from the Rochefort Trappist brewery. Dark colour, full and very impressive taste. Strong plum, raisin, and black currant palate, with ascending notes of vinousness and other complexities."
-  },
-  {
-    "alcohol": 7,
-    "name": "Chimay Rouge",
-    "description": "This Trappist beer possesses a beautiful coppery colour that makes it particularly attractive. Topped with a creamy head, it gives off a slight fruity apricot smell from the fermentation. The aroma felt in the mouth is a balance confirming the fruit nuances revealed to the sense of smell. This traditional Belgian beer is best savoured at cellar temperature "
-  }
-];
+//const beers = [];
 
 const criteria = [
   { name: "name", label: "Alphabetical" },
@@ -39,7 +13,8 @@ class BeerList extends LitElement {
 
   constructor() {
     super();
-    this.beers = beers;
+    this.beers = [];
+    this._getData();
     this.criterium = criteria[0].name;
   }
 
@@ -84,6 +59,16 @@ class BeerList extends LitElement {
 
   _descendingChange() {
     this.descendingSort = this.shadowRoot.querySelector('#descending').checked;
+  }
+
+  async _getData() {
+    try {
+      const response = await fetch('./data/beers/beers.json');
+      this.beers = await response.json();
+    }
+    catch (err) {
+      console.log('Fetch failed', err);
+    }
   }
 
   static get styles() {
@@ -133,7 +118,12 @@ class BeerList extends LitElement {
               .sort((a, b) => this._beerSorter(a, b))
               .map((beer) => {
                 return html`
-                    <beer-list-item name="${beer.name}" description="${beer.description}">
+                    <beer-list-item
+                      id="${beer.id}"
+                      name="${beer.name}"
+                      description="${beer.description}"
+                      img="${beer.img}"
+                      alcohol="${beer.alcohol}">
                     </beer-list-item>
                 `;
                 })
