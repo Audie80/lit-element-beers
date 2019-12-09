351d4c8c6bb99d33c43ac9fc0f4a11970a1c98e2
Audie <audie.menut@gmail.com>
2019-12-09 17:19:10 +0100

    Step 7

--- app/elements/beer-list-item.js
+++ app/elements/beer-list-item.js
@@ -21,7 +21,9 @@ class BeerListItem extends LitElement {
       </style>
       <div id="${this.id}" class="beer clearfix">
         <img class="float-right el-img" src="./data/beers/${this.img}">
-        <h2 class="el-name">${this.name}</h2>
+        <a href="/beer/${this.id}">
+          <h2 class="el-name">${this.name}</h2>
+        </a>
         <p class="el-description">${this.description}</p>
         <p class="float-right el-alcohol">Alcohol content: ${this.alcohol}%</p>
       </div>
--- app/web-server.js
+++ app/web-server.js
@@ -90,6 +90,9 @@ StaticServlet.prototype.handleRequest = function(req, res) {
   var path = ('./' + req.url.pathname).replace('//','/').replace(/%(..)/g, function(match, hex){
     return String.fromCharCode(parseInt(hex, 16));
   });
+  // var path = ('./index.html').replace('//', '/').replace(/%(..)/g, function (match, hex) {
+  //   return String.fromCharCode(parseInt(hex, 16));
+  // });
   var parts = path.split('/');
   if (parts[parts.length-1].charAt(0) === '.')
     return self.sendForbidden_(req, res, path);
